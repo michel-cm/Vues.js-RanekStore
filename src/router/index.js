af90fde10c38666f148ec44bb997ad7b6fd70c76
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-26 13:50:53 -0300

    produto filter

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ProdutoItem from "../views/ProdutoItem.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,12 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/produto/:id",
+    name: "produto",
+    component: ProdutoItem,
+    props: true,
   },
 ];
 
