d8115c84a943f6b095c5c102662693aa8424d4ac
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-26 08:59:26 -0300

    Busca criado

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <form>
+    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="buscar"
+      @click.prevent="buscarProdutos"
+    />
+    {{ busca }}
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
+};
 </script>
 
 <style scoped></style>
