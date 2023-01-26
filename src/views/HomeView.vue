5a266ba905ccf494211f97ecc25bb03800eeaaec
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-25 21:24:58 -0300

    Home base

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Compre ou Venda</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
   name: "HomeView",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
