974995da15690a8ee6d5f922d0a76863fd8064e5
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-27 08:26:55 -0300

    user estilo

--- src/views/Usuario/UsuarioView.vue
+++ src/views/Usuario/UsuarioView.vue
@@ -10,7 +10,7 @@
             >Editar Usuário</router-link
           >
         </li>
-        <li><button @click.prevent="deslogar">Deslogar</button></li>
+        <li><button @click="deslogar">Deslogar</button></li>
       </ul>
     </nav>
     <transition mode="out-in">
@@ -29,3 +29,46 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.usuario {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  gap: 30px;
+  padding: 20px;
+}
+
+@media screen and (max-width: 500px) {
+  .usuario {
+    grid-template-columns: 1fr;
+    margin: 0px auto;
+  }
+}
+
+.sidenav a,
+.sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+  background: #87f;
+  color: #fff;
+}
+
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+}
+</style>
